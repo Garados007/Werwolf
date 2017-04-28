fca36e7ae7da7c734926edd334ab19bb41295b36
Max Brauer <ma.brauer@live.de>
2017-04-28 14:59:50 +0200

    added config and gitignore

---
+++ config.template.php
@@ -0,0 +1,17 @@
+<?php
+//This is a template config file
+//To get your version, just copy this file to 'config.php' and do your settings.
+
+
+//Database
+define('db_server', 'localhost');
+define('db_user', 'root');
+define('db_pw', '');
+
+//Runtime Setting
+define('release_mode', false);
+
+//Server Setting
+define('uri_host', 'http://localhost/');
+define('uri_path', 'werwolf/'); //if you leave this blank, then the root of the webspace is the root of this project
+
