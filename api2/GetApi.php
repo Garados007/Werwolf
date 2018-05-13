e4706f72b8d2ab0fca6e623fd7684716c5c09609
Max Brauer <ma.brauer@live.de>
2018-05-13 19:27:49 +0200

    add protection to GetApi api class using the permission system

--- api2/GetApi.php
+++ api2/GetApi.php
@@ -20,10 +20,18 @@ class GetApi extends ApiBase {
     }
 
     public function getGroup() {
+        if (($result = $this->getAccount()) !== true)
+            return $this->wrapError($result);
         if (($result = $this->getData(array(
             'group' => 'int'
         ))) !== true)
             return $this->wrapError($result);
+        $this->inclPerm();
+        if (($result = Permission::canGetGroupData(
+            $this->account['id'],
+            $this->formated['group']
+        )) !== true) return $this->wrapError($result);
+
         $this->inclDb('Group');
         $group = Group::create($this->formated['group']);
         if ($group === null)
@@ -32,11 +40,19 @@ class GetApi extends ApiBase {
     }
 
     public function getUser() {
+        if (($result = $this->getAccount()) !== true)
+            return $this->wrapError($result);
         if (($result = $this->getData(array(
             'group' => 'int',
             'user' => 'int'
         ))) !== true)
             return $this->wrapError($result);
+        $this->inclPerm();
+        if (($result = Permission::canGetGroupData(
+            $this->account['id'],
+            $this->formated['group']
+        )) !== true) return $this->wrapError($result);
+
         $this->inclDb('User');
         foreach (User::loadAllUserByGroup($this->formated['group']) as $user)
             if ($user->user == $this->formated['user'])
@@ -45,20 +61,36 @@ class GetApi extends ApiBase {
     }
 
     public function getUserFromGroup() {
+        if (($result = $this->getAccount()) !== true)
+            return $this->wrapError($result);
         if (($result = $this->getData(array(
             'group' => 'int'
         ))) !== true)
             return $this->wrapError($result);
+        $this->inclPerm();
+        if (($result = Permission::canGetGroupData(
+            $this->account['id'],
+            $this->formated['group']
+        )) !== true) return $this->wrapError($result);
+
         $this->inclDb('User');
         $user = User::loadAllUserByGroup($this->formated['group']);
         return $this->wrapResult($user);
     }
 
     public function getChatRoom() {
+        if (($result = $this->getAccount()) !== true)
+            return $this->wrapError($result);
         if (($result = $this->getData(array(
             'chat' => 'int'
         ))) !== true)
             return $this->wrapError($result);
+        $this->inclPerm();
+        if (($result = Permission::canGetChatData(
+            $this->account['id'],
+            $this->formated['chat']
+        )) !== true) return $this->wrapError($result);
+
         $this->inclDb('ChatRoom');
         $chat = ChatRoom::create($this->formated['chat']);
         if ($chat === null)
@@ -67,22 +99,39 @@ class GetApi extends ApiBase {
     }
 
     public function getChatRooms() {
+        if (($result = $this->getAccount()) !== true)
+            return $this->wrapError($result);
         if (($result = $this->getData(array(
             'game' => 'int'
         ))) !== true)
             return $this->wrapError($result);
+        $this->inclPerm();
+
         $this->inclDb('ChatRoom');
         $result = array();
-        foreach (ChatRoom::getAllChatRoomIds($this->formated['group']) as $id)
-            $result[] = ChatRoom::create($id);
+        foreach (ChatRoom::getAllChatRoomIds($this->formated['game']) as $id) {
+            $chat = ChatRoom::create($id);
+            if ((Permission::canGetChatData(
+                $this->account['id'],
+                $chat->id
+            )) === true) $result[] = $chat;
+        }
         return $this->wrapResult($result);
     }
 
     public function getChatEntrys() {
+        if (($result = $this->getAccount()) !== true)
+            return $this->wrapError($result);
         if (($result = $this->getData(array(
             'chat' => 'int'
         ))) !== true)
             return $this->wrapError($result);
+        $this->inclPerm();
+        if (($result = Permission::canGetChatData(
+            $this->account['id'],
+            $this->formated['chat']
+        )) !== true) return $this->wrapError($result);
+
         $this->inclDb('ChatEntry');
         $result = ChatEntry::loadAllEntrys(
             $this->formated['chat'], 
@@ -92,11 +141,19 @@ class GetApi extends ApiBase {
     }
 
     public function getVotes() {
+        if (($result = $this->getAccount()) !== true)
+            return $this->wrapError($result);
         if (($result = $this->getData(array(
             'chat' => 'int',
             'voteKey' => [ 'regex', '/^.{1,5}$/']
         ))) !== true)
             return $this->wrapError($result);
+        $this->inclPerm();
+        if (($result = Permission::canGetChatData(
+            $this->account['id'],
+            $this->formated['chat']
+        )) !== true) return $this->wrapError($result);
+
         $this->inclDb('VoteEntry', 'VoteSetting');
         $voting = VoteSetting::create(
             $this->formated['chat'],
