25f4d906b38e8c9aa11ad3c6851fa8a1d83f36ab
Max Brauer <ma.brauer@live.de>
2018-05-15 14:37:22 +0200

    add definition for result type

---
+++ ui/elm/Game/Types/Response.elm
@@ -0,0 +1,69 @@
+module Game.Types.Response exposing (..)
+
+import Game.Types.Types exposing (..)
+import Game.Types.CreateOptions exposing (..)
+import Dict exposing (Dict)
+import Json.Encode exposing (Value)
+
+type alias Response =
+    { info : ResponseInfo
+    , result : Result
+    }
+
+type alias ResponseInfo =
+    { class : String
+    , method : String
+    , request : Dict String Value
+    }
+
+type Result
+    = RGet ResultGet
+    | RConv ResultConv
+    | RControl ResultControl
+    | RInfo ResultInfo
+    | RMulti ResultMulti
+    | RError ErrorInfo
+
+type ResultGet
+    = GetUserStats UserStat
+    | GetOwnUserStat UserStat
+    | GetGroup Group
+    | GetUser User
+    | GetUserFromGroup (List User)
+    | GetMyGroupUser (List User)
+    | GetChatRoom Chat
+    | GetChatRooms (List Chat)
+    | GetChatEntrys (List ChatEntry)
+    | GetVotes (List Vote)
+
+type ResultConv
+    = LastOnline (List (Int, Int))
+    | GetUpdatedGroup (Maybe Group)
+    | GetChangedVotings (List Voting)
+    | GetNewChatEntrys (List ChatEntry)
+    | GetNewVotes (List Vote)
+
+type ResultControl
+    = CreateGroup Group
+    | JoinGroup Group
+    | ChangeLeader Group
+    | StartNewGame Game
+    | NextPhase Game
+    | PostChat ChatEntry
+    | StartVoting Voting
+    | FinishVoting
+    | Vote_ Vote
+
+type ResultInfo
+    = InstalledGameTypes (List String)
+    | CreateOptions_ CreateOptions
+    | InstalledRoles (List String)
+    | Rolesets (List String)
+
+type ResultMulti
+    = Multi (List Response)
+
+type alias ErrorInfo =
+    { key : String
+    , info : String
+    }
