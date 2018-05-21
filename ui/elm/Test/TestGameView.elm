baea463768ed9cccd268388504b7bdfb8a03f900
Max Brauer <ma.brauer@live.de>
2018-05-22 00:37:36 +0200

    enable url args for test game view

--- ui/elm/Test/TestGameView.elm
+++ ui/elm/Test/TestGameView.elm
@@ -3,9 +3,12 @@ module Test.TestGameView exposing (main)
 import Game.UI.GameView as GameView exposing (..)
 import Game.Utils.Network as Network exposing (..)
 
-import Html exposing (program, Html, div)
+import Html exposing (Html, div)
 import Html.Attributes exposing (style)
 import Task exposing (succeed, perform)
+import Navigation exposing (program,Location)
+import String exposing (slice)
+import Http exposing (decodeUri)
 
 type alias Model =
     { network : Network
@@ -15,19 +18,24 @@ type alias Model =
 type Msg
     = MNetwork NetworkMsg
     | MGameView GameViewMsg
+    | None
 
 main : Program Never Model Msg
-main = program 
+main = program locChange
     { init = init
     , view = view
     , update = update
     , subscriptions = subscriptions
     }
 
-init : (Model, Cmd Msg)
-init = 
-    let 
-        (gameView, gcmd) = GameView.init 3 1
+locChange : Location -> Msg
+locChange loc = None
+
+init : Location -> (Model, Cmd Msg)
+init loc = 
+    let (group, user) = Maybe.withDefault (3,1) <|
+            parse <| loc.search
+        (gameView, gcmd) = GameView.init group user
     in (Model network gameView, Cmd.map MGameView gcmd)
 
 view : Model -> Html Msg
@@ -81,9 +89,46 @@ update msg model =
                     let
                         (ng, gcmd) = GameView.update gmsg model.gameView
                     in ({ model | gameView = ng }, Cmd.map MGameView gcmd) 
+        None -> (model, Cmd.none)
 
 subscriptions : Model -> Sub Msg
 subscriptions model = Sub.batch
     [ Sub.map MNetwork <| Network.subscriptions model.network 
     , Sub.map MGameView <| GameView.subscriptions model.gameView
     ]        
+
+parse : String -> Maybe (Int, Int)
+parse text = case slice 0 1 text of
+    "" -> Nothing
+    "?" -> parse <| String.dropLeft 1 text
+    _ ->
+        let tiles : List (String, String)
+            tiles = String.split "&" text 
+                |> List.map (String.split "=")
+                |> List.filterMap
+                    (\list -> case list of
+                        [k,v] -> Just (k,v)
+                        _ -> Nothing
+                    )
+                |> List.map (\(k,v) -> (decodeUri k, decodeUri v))
+                |> List.filterMap
+                    (\entry -> case entry of
+                        (Just k, Just v) -> Just (k,v)
+                        _ -> Nothing
+                    )
+        in case find (\(k,v) -> k == "group") tiles of
+            Nothing -> Nothing
+            Just (_, group) -> case find (\(k,v) -> k == "user") tiles of
+                Nothing -> Nothing
+                Just (_, user) -> case (String.toInt group, String.toInt user) of
+                    (Ok g, Ok u) -> Just (g, u)
+                    _ -> Nothing
+
+find : (a -> Bool) -> List a -> Maybe a
+find func list = case list of
+    [] -> Nothing
+    l :: ls ->
+        if func l
+        then Just l
+        else find func ls
+
