a41121609cce6e7e2d3b773d1ca50bae9b81ccdc
Max Brauer <ma.brauer@live.de>
2018-06-10 16:30:56 +0200

    add a bunch of english date and time formats

--- ui/elm/Game/Utils/Dates.elm
+++ ui/elm/Game/Utils/Dates.elm
@@ -11,6 +11,19 @@ type DateTimeFormat
     | DD_MM_YYYY_H24_M
     | H24_M_S
     | H24_M
+    | YYYY_MM_DD_HH_MM_SS
+    | YYYY_MM_DD_HH_MM
+    | YYYY_MM_DD
+    -- | HH_MM_SS -- equals H24_M_S
+    | S_D_M_Y_H12_M_S
+    | S_D_M_Y_H12_M
+    | S_D_M_Y
+    | H12_M_S
+    | H12_M
+    | S_M_D_Y_H12_M_S
+    | S_M_D_Y_H12_M
+    | S_M_D_Y
+
 
 all : Dict String DateTimeFormat
 all = Dict.fromList
@@ -19,6 +32,17 @@ all = Dict.fromList
     , ("DD_MM_YYYY_H24_M", DD_MM_YYYY_H24_M)
     , ("H24_M_S", H24_M_S)
     , ("H24_M", H24_M)
+    , ("YYYY_MM_DD_HH_MM_SS", YYYY_MM_DD_HH_MM_SS)
+    , ("YYYY_MM_DD_HH_MM", YYYY_MM_DD_HH_MM)
+    , ("YYYY_MM_DD", YYYY_MM_DD_HH_MM_SS)
+    , ("S_D_M_Y_H12_M_S", S_D_M_Y_H12_M_S)
+    , ("S_D_M_Y_H12_M", S_D_M_Y_H12_M)
+    , ("S_D_M_Y", S_D_M_Y)
+    , ("H12_M_S", H12_M_S)
+    , ("H12_M", H12_M)
+    , ("S_M_D_Y_H12_M_S", S_M_D_Y_H12_M_S)
+    , ("S_M_D_Y_H12_M", S_M_D_Y_H12_M)
+    , ("S_M_D_Y", S_M_D_Y)
     ]
 
 ex : Int -> Int -> String
@@ -52,6 +76,8 @@ convert format time =
         h = (truncate (inHours time)) % 24
         min = (truncate (inMinutes time)) % 60
         s = (truncate (inSeconds time)) % 60
+        h12 = if (h % 12) == 0 then 12 else h % 12
+        ampm = if h<12 then "am" else "pm"
     in case format of
         DD_MM_YYYY -> 
             (ex 2 d) ++ "." ++ (ex 2 m) ++ "." ++ (ex 4 y)
@@ -65,3 +91,31 @@ convert format time =
             (ex 2 h) ++ ":" ++ (ex 2 min) ++ ":" ++ (ex 2 s)
         H24_M -> 
             (ex 2 h) ++ ":" ++ (ex 2 min) 
+        YYYY_MM_DD_HH_MM_SS ->
+            (ex 4 y) ++ "-" ++ (ex 2 m) ++ "-" ++ (ex 2 d) ++ " "
+            ++ (ex 2 h) ++ ":" ++ (ex 2 min) ++ ":" ++ (ex 2 s)
+        YYYY_MM_DD_HH_MM ->
+            (ex 4 y) ++ "-" ++ (ex 2 m) ++ "-" ++ (ex 2 d) ++ " "
+            ++ (ex 2 h) ++ ":" ++ (ex 2 min)
+        YYYY_MM_DD ->
+            (ex 4 y) ++ "-" ++ (ex 2 m) ++ "-" ++ (ex 2 d)
+        S_D_M_Y_H12_M_S ->
+            (ex 2 d) ++ "/" ++ (ex 2 m) ++ "/" ++ (ex 4 y) ++ " "
+            ++ (ex 2 h12) ++ ":" ++ (ex 2 min) ++ ":" ++ (ex 2 s) ++ " " ++ ampm
+        S_D_M_Y_H12_M ->
+            (ex 2 d) ++ "/" ++ (ex 2 m) ++ "/" ++ (ex 4 y) ++ " "
+            ++ (ex 2 h12) ++ ":" ++ (ex 2 min) ++ " " ++ ampm
+        S_D_M_Y ->
+            (ex 2 d) ++ "/" ++ (ex 2 m) ++ "/" ++ (ex 4 y)
+        H12_M_S ->
+            (ex 2 h12) ++ ":" ++ (ex 2 min) ++ ":" ++ (ex 2 s) ++ " " ++ ampm
+        H12_M ->
+            (ex 2 h12) ++ ":" ++ (ex 2 min) ++ " " ++ ampm
+        S_M_D_Y_H12_M_S ->
+            (ex 2 m) ++ "/" ++ (ex 2 d) ++ "/" ++ (ex 4 y) ++ " "
+            ++ (ex 2 h12) ++ ":" ++ (ex 2 min) ++ ":" ++ (ex 2 s) ++ " " ++ ampm
+        S_M_D_Y_H12_M ->
+            (ex 2 m) ++ "/" ++ (ex 2 d) ++ "/" ++ (ex 4 y) ++ " "
+            ++ (ex 2 h12) ++ ":" ++ (ex 2 min) ++ " " ++ ampm
+        S_M_D_Y ->
+            (ex 2 m) ++ "/" ++ (ex 2 d) ++ "/" ++ (ex 4 y)
